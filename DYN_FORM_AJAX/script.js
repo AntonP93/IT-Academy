78387e7891ad6124c42e95bcea92034330f275d8
Anton Plastun <p.anton070793@gmail.com>
2022-11-06 16:38:02 +0300

    file: my_game new part

--- DYN_FORM_AJAX/script.js
+++ DYN_FORM_AJAX/script.js
@@ -22,23 +22,74 @@
 //   {label:'Описание сайта:',kind:'memo',name:'description'},
 //   {caption:'Опубликовать',kind:'submit'},
 // ];
-function ajaxForm(){
-    let arrFormAJAX = ['https://fe.it-academy.by/Examples/dyn_form_ajax/formDef1.json','https://fe.it-academy.by/Examples/dyn_form_ajax/formDef2.json'];
+let arr =[];
+// function ajaxForm(){
+//     let arrFormAJAX = ['https://fe.it-academy.by/Examples/dyn_form_ajax/formDef1.json','https://fe.it-academy.by/Examples/dyn_form_ajax/formDef2.json'];
 
-    for(let i = 0; i < arrFormAJAX.length; i++){
-        $.ajax(`${arrFormAJAX[i]}`,
-        { type:'GET', dataType:'text', success: dataLoaded }
-        );
-    }    
+//     for(let i = 0; i < arrFormAJAX.length; i++){
+//         $.ajax(`${arrFormAJAX[i]}`,
+//         { type:'GET', dataType:'text', success: dataLoaded }
+//         );
+//     }    
+// }
+// let formDef1 = 'https://fe.it-academy.by/Examples/dyn_form_ajax/formDef1.json';
+//     $.ajax(formDef1,
+//     { type:'GET', dataType:'text', success:dataLoaded }
+//     );
+// let formDef2 = 'https://fe.it-academy.by/Examples/dyn_form_ajax/formDef2.json';
+//     $.ajax(formDef2,
+//     { type:'GET', dataType:'text', success:dataLoaded }
+//     );        
+// function dataLoaded(data){
+//     let dataForm = JSON.parse(data);
+//     arr.push(dataForm);
+//     console.log(arr)
+//     if(arr.length >=2){
+//         formAdd(arr)
+//     }
+// }
+let formDef1 = 'https://fe.it-academy.by/Examples/dyn_form_ajax/formDef1.json';
+let formDef2 = 'https://fe.it-academy.by/Examples/dyn_form_ajax/formDef2.json';
+let arrNew = [];
+function req(formDef){
+    var request = new XMLHttpRequest();
+    request.open('GET',formDef,false);
+    request.send();
+    request.responseType = 'text';
+   
+    request.onload = function() {
+        var arrJSON = request.response;
+        let arr1 = JSON.parse(arrJSON);
+        if(request.readyState === 4){
+            formAdd(arr1)
+        }
+        
+    } 
 }
-function dataLoaded(data) {
-    let dataForm = JSON.parse(data);
+req(formDef1)
+req(formDef2)
+
+
+
+
+// function dataLoaded(data){
+//     let dataForm = JSON.parse(data);
+//     arr.push(dataForm);
+//     console.log(arr)
+//     if(arr.length >=2){
+//         formAdd(arr)
+//     }
+// }
+
+// formAdd(arrNew9);
+// formAdd(arrNew[5]);
+function formAdd(arr) {
+    console.log(arr)
     let formIn = document.createElement('form');
     formIn.method = 'POST';
     formIn.action = "https://fe.it-academy.by/TestForm.php"
     document.body.append(formIn);
-
-    dataForm.forEach( element => {
+    arr.forEach( element => {
         let divIn = document.createElement('rt');
         if(element.label){
         divIn.innerText = element.label;
@@ -84,7 +135,6 @@ function dataLoaded(data) {
         divIn.append(input);     
     });   
 }
-ajaxForm();
 
 
 
--- my_game/script.js
+++ my_game/script.js
@@ -1,40 +1,62 @@
 const CVS = document.querySelector('.field');
 const context = CVS.getContext('2d');
-
-const pageWidth = document.documentElement.scrollWidth
-const pageHeight = document.documentElement.scrollHeight
-
+const healthPlayer1 = document.querySelector('.health_player1');
+const healthPlayer2 = document.querySelector('.health_player2');
+const pageWidth = document.documentElement.scrollWidth;
+const pageHeight = document.documentElement.scrollHeight;
+const timerRound = 60;
+console.log(healthPlayer1,healthPlayer2)
 const coeffWidth = 0.8;
 const coeffhight = 0.65;
 
 const grav = 0.2;
-
+const  hit = 10;
 // canvasField.width = pageWidth * coeffWidth;   
 // canvasField.height = pageHeight * coeffWidth;
 
 
 
-CVS.width = 1024;
+CVS.width = 1500;
 CVS.height = 576;
 
 context.fillRect(0,0,CVS.width, CVS.height);
+// let img_CVS = new Image
 
-
-class Sprite{
-    constructor({position,speed}){
+//спрайт игрока
+class Player{
+    constructor({position,speed,color,vector}){
         this.position = position;
-        this.speed = speed ;
+        this.speed = speed;
         this.height = 150;
         this.width = 50;
+        this.health = 100;
+        this.attackArm = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            width: 120,
+            height: 50 
+            
+        };
+        this.vector = vector
+        this.color = color;
+        this.attack = false
     }
 
     view(){
-        context.fillStyle = 'green';
+        context.fillStyle = this.color;
         context.fillRect(this.position.x, this.position.y, this.width, this.height);
+        if(this.attack){
+        context.fillStyle = 'green';
+        context.fillRect(this.attackArm.position.x,this.attackArm.position.y, this.attackArm.width,this.attackArm.height)
+        }
     }
 
     update(){
         this.view();
+        this.attackArm.position.x = this.position.x - this.vector
+        this.attackArm.position.y = this.position.y
         this.speed.y += grav;
         this.position.x += this.speed.x
         this.position.y +=  this.speed.y ;
@@ -51,9 +73,35 @@ class Sprite{
             this.position.x = CVS.width-this.width;    
         }    
     }
+    attacking(){
+        this.attack = true
+        setTimeout(()=>{
+            this.attack = false
+        },100)
+    }
+    hit(){
+        this.health = this.health - hit;
+    }
 }
 
-const Player1 = new Sprite({
+class Sprite {
+    constructor({position,imgSrc}){
+        this.image = new Image();
+        this.position = position;
+        this.image.src = imgSrc;
+        
+        
+    }
+    view(){
+        context.drawImage(this.image,this.position.x, this.position.y,CVS.width,CVS.height)        
+    }
+
+    update(){
+        this.view();   
+    }
+}
+// характеристика игрока 1
+const Player1 = new Player({
     position: {
         x:50,
         y:0
@@ -62,9 +110,12 @@ const Player1 = new Sprite({
        x:0,
        y:0
     },
-});
+    color: 'red',
+    vector: 0
 
-const Player2 = new Sprite({
+});
+// характеристика игрока 2
+const Player2 = new Player({
     position: {
         x:800,
         y:0
@@ -73,28 +124,58 @@ const Player2 = new Sprite({
        x:0,
        y:0
     },
+    color: 'brown',
+    vector:70
+
 });
 
+//бэкграунд
+const backgrnd = new Sprite({
+    position:{
+        x:0,
+        y:0
+    },
+    imgSrc:'img/MK_background.jpg' 
 
+})
+
+// обработчики ударов и передвижения
 document.addEventListener('keydown',(EO)=>{
     EO=EO||window.event;
     switch(EO.key) {
         case 'd':
-            console.log(EO.key)
-            Player1.speed.x = 2;
+  
+            Player1.speed.x = 5;
         break
         case 'a':
-            console.log(EO.key)
-            Player1.speed.x = -2;
+  
+            Player1.speed.x = -5;
+        break 
+        case 'w':
+
+            Player1.speed.y = -10;
         break 
         case 'ArrowRight':
-            console.log(EO.key)
-            Player2.speed.x = 2;
+
+            Player2.speed.x = 5;
         break
         case 'ArrowLeft':
-            console.log(EO.key)
-            Player2.speed.x = -2;
-        break      
+
+            Player2.speed.x = -5;
+        break
+        case 'ArrowUp':
+
+            Player2.speed.y = -10;
+        break
+        case 's':
+
+            Player1.attacking();
+        break
+        case 'ArrowDown':
+
+            Player2.attacking();
+        break
+
     }
    
 })
@@ -103,35 +184,59 @@ document.addEventListener('keyup',(EO)=>{
     EO=EO||window.event;
     switch(EO.key) {
         case 'd':
-            console.log(EO.key)
+
             Player1.speed.x = 0;
         break
         case 'a':
-            console.log(EO.key)
+
             Player1.speed.x = 0;
         break
         case 'ArrowRight':
-            console.log(EO.key)
+
             Player2.speed.x = 0;
         break
         case 'ArrowLeft':
-            console.log(EO.key)
+
             Player2.speed.x = 0;
+        break
+        case 's':
+
+            Player1.attack = false;
+        break
+        case 'ArrowDown':
+  
+            Player2.attack = false;
         break     
     }    
     }
    
 )
 
+//анимация движения
 function tick(){
-    console.log('go')
     context.fillStyle = 'black'
     context.fillRect(0,0,CVS.width,CVS.height);
+
+    backgrnd.update();
+    
     Player1.update();
     Player2.update();
+
+    //регистрация удара
+    if(Player1.attackArm.position.x + Player1.attackArm.width>= Player2.position.x && Player1.attackArm.position.x <= Player2.position.x + Player2.width && Player1.attackArm.position.y + Player1.attackArm.height >= Player2.position.y && Player1.attackArm.position.y <= Player2.position.y + Player2.height && Player1.attack ){
+        console.log('hitPlayer1')
+        Player2.hit()
+        healthPlayer2.style.width = Player2.health+'%';
+        Player1.attack = false;
+    }
+    if(Player2.attackArm.position.x + Player2.attackArm.width>= Player1.position.x && Player2.attackArm.position.x <= Player1.position.x + Player1.width && Player2.attackArm.position.y + Player2.attackArm.height >= Player1.position.y && Player2.attackArm.position.y <= Player1.position.y + Player1.height && Player2.attack ){
+        console.log('hitPlayer2')
+        Player1.hit()
+        healthPlayer1.style.width = Player1.health+'%';
+        Player2.attack = false;
+    }
     requestAnimationFrame(tick);
 }
-
 requestAnimationFrame(tick);
 
 
